3094c287905db09d775a828ce4745d579a781cea
Logan Cooper <logancooper@bellsouth.net>
2021-05-14 10:55:39 -0400

    added party supplies print function

--- party.py
+++ party.py
@@ -6,9 +6,7 @@
 #Phone Charge
 #Bullets
 
-
-
-
+#print party supplies
 class Party():
     def __init__(self, party_members, money, food, hand_sanitizer, fuel, phone_charge, bullets):
         self.party_members = party_members
@@ -23,3 +21,6 @@ class Party():
     def print_party_status(self):
         for character in self.party_members:
             character.print_stats()
+
+    def print_party_supplies(self):
+        print("---------Party Supplies---------\nMoney: %s\nFood: %d\nHand Sanitizer: %d\nFuel: %d\nPhone Charge: %d\nBullets: %d" % (self.money,self.food,self.hand_sanitizer,self.fuel,self.phone_charge,self.bullets))
